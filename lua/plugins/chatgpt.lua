dce505d0d9c896918c4555c0051bcc90d315e33b
MarcusL11 <marclert@gmail.com>
2024-04-10 15:35:21 +0700

    update smooth scrolling and tailwind LSP

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -2,7 +2,9 @@ return {
   "jackMort/ChatGPT.nvim",
   event = "VeryLazy",
   config = function()
-    require("chatgpt").setup()
+    require("chatgpt").setup({
+      api_key_cmd = "pass show chatgpt-nvim",
+    })
   end,
   dependencies = {
     "MunifTanjim/nui.nvim",
@@ -11,3 +13,6 @@ return {
     "nvim-telescope/telescope.nvim",
   },
 }
+
+--  Initialized empty Git repository in /Users/marcuslertkomolsuk/.password-store/.git/
+--  to clone; git clone git@github.com:marcus/pwd-store.git .password-store
